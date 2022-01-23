4eaaa271296ec1444e206af1800118af155e96f5
James Meegan <jmeegan2@owlmail.harford.edu>
2022-01-22 23:06:02 -0500

    declaring variables

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // The Variables
 const searchbar = document.querySelector('searchbar-container');
 const profilecontainer = docuemnt.querySelector('.profile-container')
+const root = document.documentElement.style;
+const get = (param)=> document.getElementById('${param}');
+const url = 'https://api.github.com/users/';                      //linking the github API ***********
+const noresults = get('no-results')
+const btnmode = get('btn-mode')
+const modetext = get('mode-text')
+const modeicon = get('mode-icon')
+const btnsubmit = get('submit')
+const input = get('input')
+const avatar = get('avatar')
+const userName = get('name')
+const user = get('user')
+const date = get('date')
+const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+const bio = get('bio')
+const repos = get('repos')
+const followers = get('followers')
+const following = get('following')
+const user_location = get('location')
+const page = get('page')
+const twitter = get('twitter')
+const company = get('company')
+let darkMode = false;
+//buttons 
